a9e2efcddb146013708775993878614a919dd163
Krishna <krishna858606@gmail.com>
2021-06-22 13:25:51 +0530

    cxc

---
+++ springbootrest/src/main/java/com/example/demo/AlienResource.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AlienResource {
+	
+	@Autowired
+	AlienRepository repo;
+    @GetMapping("aliens")
+	public List<Alien> getAliens(){
+		List<Alien> aliens=repo.findAll();
+		return aliens;
+	}
+}
