d13eb409155920ca045c738aef5ff6a541349b5a
Krishna <krishna858606@gmail.com>
2021-06-22 13:26:51 +0530

    dsfdf

---
+++ testjee/src/com/example/demo/TestServlet.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/testservlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  System.out.println(request.getClass().getName());
+      System.out.println(response.getClass().getName());
+      
+      System.out.println(this);
+      System.out.println(Thread.currentThread().getId());
+		response.getWriter().append("Served at: ");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
