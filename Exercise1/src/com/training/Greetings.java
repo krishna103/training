938f7afef424cf0d6ec347df887d78c649af60aa
Krishna <krishna858606@gmail.com>
2021-06-22 13:12:25 +0530

    dfdfd

---
+++ Exercise1/src/com/training/Greetings.java
@@ -0,0 +1,8 @@
+package com.training;
+
+public class Greetings {
+      public String getMessage()
+      {
+    	  return "Welcome to Java Programming";
+      }
+}
---
+++ Exercise1/src/com/training/TestGreetings.java
@@ -0,0 +1,10 @@
+package com.training;
+
+public class TestGreetings {
+
+	public static void main(String[] args) {
+         Greetings o1=new Greetings();
+         System.out.println(o1.getMessage());
+	}
+
+}
